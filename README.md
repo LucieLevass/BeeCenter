3f166dccc4fd5066f559b673f845834032a74804
Lucie Levasseur <lucie.levasseur10@gmail.com>
2023-01-12 20:31:43 +0100

    Creation BDD

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BeeCenter
+
+Faire un npm init
+Faire un npm install express et npm install mysql
+
+Pour créer la base de donnée, lancer le server et executer la route localhost:5000/createDatabase
+Si erreur d'acces, changer le champs password dans sqlConnection.js et mettre le mot de passe de votre root
+
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const database = require('./sqlConnection');
+
+const app = express();
+
+app.listen(5000, () => {
+    console.log(`Server is up and running on 5000 ...`);
+});
+
+
+
+app.get("/createDatabase", (req, res) => {
+
+    let databaseName = "beecenter";
+
+    let createQuery = `CREATE DATABASE ${databaseName}`;
+
+    // use the query to create a Database.
+    database.query(createQuery, (err) => {
+        if (err) throw err;
+
+        console.log("Database Created Successfully !");
+
+        let useQuery = `USE ${databaseName}`;
+        database.query(useQuery, (error) => {
+            if (error) throw error;
+
+            console.log("Using Database");
+
+            return res.send(
+                `Created and Using ${databaseName} Database`);
+        })
+    });
+});
