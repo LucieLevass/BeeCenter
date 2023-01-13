05b627c5a07a9b5f9488e7124a39d78e9862ac61
Damien <damien@LAPTOP-OE4I5EUT.localdomain>
2023-01-13 20:05:01 +0100

    ajout de plantes dans la base de données

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const database = require('./sqlConnection');
 
 const app = express();
 
-app.listen(5000, () => {
+app.listen(5222, () => {
     console.log(`Server is up and running on 5000 ...`);
 });
 
